f17f1d79cc231972d6443ca8d9f414b72d091128
Salsabeel-Abuelmagd <116510149+Salsabeel-Abuelmagd@users.noreply.github.com>
2022-12-13 20:55:51 +0200

    Update soso.cs

--- soso.cs
+++ soso.cs
@@ -3,40 +3,5 @@ namespace Soso;
 public class Soso{
 private static void Main(){
 
-//reverse a string
-string str="";
-string reverse ="";
-int len=0;
-Console.WriteLine("Enter a string");
-str = Console.ReadLine();
-len=str.Length -1;
-for(;len>=0;len--){
-reverse+=str[len];
-} 
-Console.WriteLine(reverse);
-
-//count of a's 
-Console.WriteLine("Enter a string");
-String st=Console.ReadLine();
-var count=0;
-for(int i=0;i<st.Length;i++){
-if (st[i]=='a'){
-    count ++;
-}
-} Console.WriteLine("count of a's in string is " + count);
-
-//Last index of a
-Console.WriteLine("Enter a string");
-String stri=Console.ReadLine();
-Console.WriteLine(stri.LastIndexOf('a'));
-
-//remove all a's
-Console.WriteLine("Enter a string");
-String value=Console.ReadLine();
-for(int i=0;i<value.Length;i++){
-if (value[i]=='a'){
-    
-}
-} Console.WriteLine(value);
 
 }}
