fe10965f0664b4c4b93e3afad7f36a649702fc27
Salsabeel-Abuelmagd <116510149+Salsabeel-Abuelmagd@users.noreply.github.com>
2022-12-13 21:06:11 +0200

    Update soso.cs

--- soso.cs
+++ soso.cs
@@ -1,18 +1,27 @@
 using System;
 namespace Soso;
-public class Soso{
-private static void Main(){
-    public class Staff : Person {
-    public double Salary;
-    public int JoinYear;
-    public ctor (string name,int age,double salary ,int joinyear):base(name,age){
-    Salary=salary;
-    JoinYear=joinyear; 
-    }
-    public override void Print(){
-     Console.WriteLine($"My name is{Name},my age is{Age},and my salary is {Salary}");
+public abstract class Person{
+
+    public string Name;
+    public int Age;
+
+    public Person(string name, int age ){
+        Name=name;
+        Age=age;
     }
 
+    public abstract void Print();
 }
+
+public class Student :Person{
+    public int Year;
+    public float Gpa;
+    public Student (string name,int age,float gpa ):base(name,age){
+        Year=year;
+        Gpa=gpa;
+    }
+    public override void Print(){
+        Console.WriteLine($"My name is{Name},my age is{Age},and gpa is {Gpa}");
+    }
 }
 }}
