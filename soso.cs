59214c55462841c079c925fac3b92680856b711a
Salsabeel-Abuelmagd <116510149+Salsabeel-Abuelmagd@users.noreply.github.com>
2022-12-13 21:03:28 +0200

    Update soso.cs

--- soso.cs
+++ soso.cs
@@ -3,7 +3,16 @@ namespace Soso;
 public class Soso{
 private static void Main(){
     public class Staff : Person {
-  public double Salary;
-  public int JoinYear;
+    public double Salary;
+    public int JoinYear;
+    public ctor (string name,int age,double salary ,int joinyear):base(name,age){
+    Salary=salary;
+    JoinYear=joinyear; 
+    }
+    public override void Print(){
+     Console.WriteLine($"My name is{Name},my age is{Age},and my salary is {Salary}");
+    }
 
+}
+}
 }}
